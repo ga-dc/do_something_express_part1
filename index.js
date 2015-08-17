86e9d8e7fac59edc1eee35ec8b8dc5b8ed014120
Matt Ficke <matthew.ficke@gmail.com>
2015-08-17 19:36:43 -0400

    add index route

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var express = require('express')
-var app = exports.app = express()
+var express = require('express');
+var app = exports.app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.listen(4000, function() {
   console.log("listening on port 4000")
-})
+});
 
 var tasks = [
   {id: 1, body: "first item", completed: false},
   {id: 2, body: "second item", completed: false},
   {id: 3, body: "third item", completed: false}
-]
+];
 
+// root returns JSON file with all tasks
 app.get('/', function(req, res) {
-  res.send("root")
-  console.log("root")
-})
+  res.json(tasks)
+});
