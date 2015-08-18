bbb9cc6336b27a23ff0b3c00100515f31cb6d5fb
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-08-17 22:50:21 -0400

    switched out code

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var express = require("express")
 var app = express()
+var bodyParser = require("body-parser")
+app.use(bodyParser.json()) //handles json post requests
+app.use(bodyParser.urlencoded({ extended: true })) // handles form submissions
+var tasks = [
+  {id: 1, body: "task1", completed: true},
+  {id: 2, body: "task2", completed: false},
+  {id: 3, body: "task3", completed: false},
+]
 
-app.set("view engine", "hbs")
-
-app.get("/", function(req, res){
-  res.send("hello world")
+app.get("/tasks", function(req, res){
+  res.json(tasks)
 })
 
-app.get("/:name", function(req, res){
+app.get("/tasks/:id", function(req, res){
   res.send("hello " + req.params.name)
 })
 
-app.post("/", function(req, res){
-  res.send("hello " + req.params.name)
+app.post("/tasks", function(req, res){
+  res.send("hello", {name: req.body.name})
 })
 
 app.listen(4000, function(){
