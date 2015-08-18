6bc6e45919e114cdc120b2445c1b1424c8fc2112
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-08-18 19:47:45 -0400

    change file name application.js to index.js because the port was not listening to the app.js.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require("express")
 var app = express()
 
+app.get('/', function(req, res){
+  res.send('Hello Cheikh');
+});
+
 app.listen(4000, function(){
   console.log("app listening on port 4000")
 })
+
+module.exports = "express"
