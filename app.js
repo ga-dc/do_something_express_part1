57a6af02fe99a4252500a570f98c25c7e4a9728e
Cameron Fry <Callifry@Camerons-MacBook-Air.local>
2015-08-17 23:18:14 -0400

    Added task show route and untested create task via post route

--- app.js
+++ app.js
@@ -7,32 +7,54 @@ app.listen(4000, function(){
 
 var tasks = [
   {
-    "id" : 1,
+    "id" : "1",
     "body" : "task 1",
-    "completed" : false
+    "completed" : "false"
   },
   {
-    "id" : 2,
+    "id" : "2",
     "body" : "task 2",
-    "completed" : false
+    "completed" : "false"
   },
   {
-    "id" : 3,
+    "id" : "3",
     "body" : "task 3",
-    "completed" : false
+    "completed" : "false"
   },
   {
-    "id" : 4,
+    "id" : "4",
     "body" : "task 4",
-    "completed" : false
+    "completed" : "false"
   },
   {
-    "id" : 5,
+    "id" : "5",
     "body" : "task 5",
-    "completed" : false
+    "completed" : "false"
   }
 ];
 
 app.get("/", function(req, res){
+  res.redirect("/tasks");
+});
+
+app.get("/tasks", function(req, res){
   res.json(tasks);
+});
+
+app.get("/tasks/:id", function(req, res){
+  res.json(tasks[req.params.id - 1].body);
+});
+
+app.post("/tasks/:id", function(req, res){
+  console.log(req.body);
+  res.send(req.body);
+  // var task_id = parseInt(req.body.id);
+  // var body = req.body.body;
+  // var completed = req.body.completed;
+  //
+  // tasks.push(
+  //   {"id" : "5",
+  //     "body" : "task 6",
+  //     "completed" : "false"
+  //   });
 });
