f9825ef1ce1865f25a09c3642b7eda8411d1e207
Preston Davies <pdavies88@gmail.com>
2015-08-17 22:32:45 -0400

    updated with post

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
+var express = require("express")
+var app = express()
+var bodyParser = require("body-parser")
 var tasks = [
-dishes = {
+{
   id: 1,
   body: "wash dishes",
   completed: false
 },
-laundry = {
+{
   id: 2,
   body: "clean clothes",
   completed: false
 },
-vacuum = {
+{
   id: 3,
   body: "vacuum carpet",
   completed: false
 },
-bed = {
+{
   id: 4,
   body: "make bed",
   completed: false
 },
-dust = {
+{
   id: 5,
   body: "dust tables",
   completed: false
 }
-]
+];
 
-var express = require("express")
-var app = express()
+app.use(bodyParser.json()) //handles json post requests
+app.use(bodyParser.urlencoded({ extended: true })) // handles form submissions
 
 app.get("/", function(req, res){
   res.json(tasks)
 })
 
 app.get("/:id", function(req, res){
-  res.json(tasks[req.params.id])
+  var todoID = (req.params.id) - 1
+  res.json(tasks[todoID])
 })
+
+app.post("/", function(req, res){
+  tasks.push(req.body)
+  res.json(req.body)
+  })
 
 app.listen(3000, function(){
   console.log("app listening at http://localhost:3000/")
