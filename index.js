381e0751561a5e93fcf71b1d788d119834550cfa
Matt Ficke <matthew.ficke@gmail.com>
2015-08-18 19:02:41 -0400

    change tasks to lists, items to tasks

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.listen(4000, function() {
 });
 
 // create an array of objects
-var tasks = [
-  {id: 1, name: "List 1", items: [
+var lists = [
+  {id: 1, name: "List 1", tasks: [
     {id: 1, body: "first item", completed: false},
     {id: 2, body: "second item", completed: false},
     {id: 3, body: "third item", completed: false}
@@ -23,35 +23,35 @@ var tasks = [
 
 // root returns JSON file with all to-do lists
 app.get('/', function(req, res) {
-  res.json(tasks);
+  res.json(lists);
 });
 
 // show a single to-do list
 app.get('/:id', function(req, res) {
   // FIXME should req.params.id be converted to int?
-  // get index value of item by id
-  var task = req.params.id - 1;
-  res.json(tasks[task].items);
+  // get index value of task by id
+  var list = req.params.id - 1;
+  res.json(lists[list].tasks);
 });
 
-// add a list to the 'tasks' array
+// add a list to the 'lists' array
 app.post('/', function(req, res) {
   // convert id to int, completed to boolean
-  tasks.push({id: tasks.length + 1, name: req.body.name, items: []});
+  lists.push({id: lists.length + 1, name: req.body.name, tasks: []});
 });
 
-// add an item to a list
+// add an task to a list
 app.post('/:id', function(req, res) {
-  var task = req.params.id - 1;
+  var list = req.params.id - 1;
   // convert id to int, completed to boolean
-  tasks[task].items.push({id: tasks[task].items.length + 1, body: req.body.body, completed: req.body.completed == "true"});
+  lists[list].tasks.push({id: lists[list].tasks.length + 1, body: req.body.body, completed: req.body.completed == "true"});
 });
 
-// update the completion status of an item
-app.put('/:id/:item_id', function(req, res) {
-  // get index value of item by id
-  var task = req.params.id - 1;
-  var item = req.params.item_id - 1;
+// update the completion status of a task
+app.put('/:id/:task_id', function(req, res) {
+  // get index value of task by id
+  var list = req.params.id - 1;
+  var task = req.params.task_id - 1;
   // change completion status, convert to boolean
-  tasks[task].items[item].completed = req.body.completed == "true"
+  lists[list].tasks[task].completed = req.body.completed == "true"
 });
