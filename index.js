8a6e85983c8eebfaf9f720a37b3a8c3875f8d7fa
Rachel Warren <raelwa@gmail.com>
2015-08-18 18:09:53 -0400

    initial commit, created variable tasks

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+var tasks = [
+      {id: 1, body: "buy groceries", completed: false},
+      {id: 2, body: "take the dog out", completed: false},
+      {id: 3, body: "feed the dog", completed: false},
+      {id: 4, body: "pet the dog", completed: false},
+      {id: 5, body: "vacuum", completed: false},
+]
