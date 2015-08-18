5905e0c9a7ce5bdb71f7a0d6a285693d516ba7ef
KristinGreenslit <kristingreenslit@gmail.com>
2015-08-17 20:57:06 -0400

    index route works

--- application.js
+++ application.js
@@ -1,38 +1,23 @@
 var express = require("express");
 var app = express();
 app.set("view engine", "hbs");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());  // for json post requests
+// app.use(bodyParser.urlencoded({extended:true}));  // for forms
 
 app.listen(3000), function(){
-  console.log("app listening on port 3000");
-});
-
-var tasks = {
-  { id: "1",
-    body:"bake a cake",
-    completed[true]:"success",
-    completed[false]:"try again"
-  }
-  { id: "2",
-    body:"walk the dog",
-    completed[true]:"success",
-    completed[false]:"try again"
-  }
-  { id: "3",
-    body:"wash the dishes",
-    completed:"boolean",
-    completed[true]:"success",
-    completed[false]:"try again"
-  }
-  { id: "4",
-    body:"shop for groceries",
-    completed:"boolean",
-    completed[true]:"success",
-    completed[false]:"try again"
-  }
-  { id: "5",
-    body:"shop for shoes",
-    completed:"boolean",
-    completed[true]:"success",
-    completed[false]:"try again"
-  }
+  console.log("app listening on port 3000");  // could be any port, 3000-5000
 };
+
+var tasks = [
+  {id: 1, body:"bake a cake", completed: true},
+  {id: 2, body:"walk the dog", completed: true},
+  {id: 3, body:"wash the dishes", completed: true},
+  {id: 4, body:"shop for groceries", completed: true},
+  {id: 5, body:"shop for shoes", completed: true},
+];
+
+// tasks#index
+app.get('/', function (req, res){
+  res.send('get index');
+});
