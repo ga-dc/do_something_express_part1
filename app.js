45f2cc6b9ba8c7f521a892b02890d42cbf9a3783
Cameron Fry <Callifry@Camerons-MacBook-Air.local>
2015-08-17 21:23:44 -0400

    Created tasks variable with 5 objects and three properties

---
+++ app.js
@@ -0,0 +1,27 @@
+var tasks = [
+  {
+    "id" : 1,
+    "body" : "task 1",
+    "completed" : false
+  },
+  {
+    "id" : 2,
+    "body" : "task 2",
+    "completed" : false
+  },
+  {
+    "id" : 3,
+    "body" : "task 3",
+    "completed" : false
+  },
+  {
+    "id" : 4,
+    "body" : "task 4",
+    "completed" : false
+  },
+  {
+    "id" : 5,
+    "body" : "task 5",
+    "completed" : false
+  }
+];
