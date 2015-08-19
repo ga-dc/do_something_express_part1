9ab9969537d45c71d5c8c01042e06011f410b127
Erica Irving <contact.eirving@gmail.com>
2015-08-18 21:30:44 -0400

    submit whats there

--- app/controllers/tasksController.js
+++ app/controllers/tasksController.js
@@ -1,18 +1,52 @@
 module.exports = {
-  app.get("/tasks", function(req, res) {
+  tasks: [
+    {id: 1, body: "Do something!", completed: true},
+    {id: 2, body: "Do something else!", completed: false},
+    {id: 3, body: "Do another thing!", completed: true},
+    {id: 4, body: "Just do it!", completed: false},
+    {id: 5, body: "Do the right thing!", completed: true}
+  ],
+
+  //tasks#index
+  index: function(req, res) {
+    res.render("index");
     res.json(tasks);
-  });
-
-  app.get("/tasks/:id", function(req, res){
-    for(var i = 0; i < tasks.length; i++){
-      if(tasks[i].id == req.params.id){
-        res.json(tasks[i]);
-      }
-    }
-  });
-
-  app.post("/tasks", function(req, res) {
-    tasks.push(req.body);
-    res.json(tasks[tasks.length - 1]);
-  });
+  }
 }
+
+//   //tasks#new
+//   app.get("/tasks/new", function(req, res) {
+//     res.send("GET form to create a new task")
+//   })
+//
+//   //tasks#create
+//   app.post("/tasks", function(req, res) {
+//     tasks.push(req.body);
+//     res.json(tasks[tasks.length - 1]);
+//   });
+//
+//   //tasks#show
+//   app.get("/tasks/:id", function(req, res){
+//     for(var i = 0; i < tasks.length; i++){
+//       if(tasks[i].id == req.params.id){
+//         res.json(tasks[i]);
+//       }
+//     }
+//   });
+//
+//   //tasks#edit
+//   app.get("/tasks/:id/edit", function(req, res) {
+//     res.send("GET form to edit task with id of " + req.params.id)
+//   })
+//
+//   //tasks#update
+//   app.put("/tasks/:id", function(req, res) {
+//     res.send("PUT edited data to task with id of " + req.params.id)
+//   })
+//
+//   //tasks#delete
+//   app.delete("tasks/:id", function(req, res){
+//     res.send("DELETE task with id of " + req.params.id)
+//   })
+//
+// }
--- application.js
+++ application.js
@@ -4,18 +4,18 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+var task = require("./app/models/task.js");
+var list = require("./app/models/list.js");
+var tasksController = require("./app/controllers/tasksController.js");
+var listsController = require("./app/controllers/listsController.js");
 
-var tasks = [
-  {id: 1, body: "Do something!", completed: true},
-  {id: 2, body: "Do something else!", completed: false},
-  {id: 3, body: "Do another thing!", completed: true},
-  {id: 4, body: "Just do it!", completed: false},
-  {id: 5, body: "Do the right thing!", completed: true}
-]
+app.set("view engine", "hbs");
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/app/views/index.html");
-});
+app.get("/", tasksController.index);
+//
+// app.get("/", function(req, res){
+//   res.sendFile(__dirname + "/app/views/index.html");
+// });
 
 app.listen(5678, function(){
   console.log("app listening on port 5678");
