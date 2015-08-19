c5a4f6289fce908c261bd38fe02a9dcc4b763110
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-18 23:11:11 -0400

    added index; moved task object to task model

--- app.js
+++ app.js
@@ -7,32 +7,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 /////////////////////////////////////////////
 
-// tasks objects
-
-var tasks = [{
-    id: 1,
-    body: "todo1",
-    completed: true
-}, {
-    id: 2,
-    body: "todo2",
-    completed: true
-}, {
-    id: 3,
-    body: "todo3",
-    completed: false
-}, {
-    id: 4,
-    body: "todo4",
-    completed: false
-}, {
-    id: 5,
-    body: "todo5",
-    completed: true
-}]
-
-/////////////////////////////////////////////
-
 app.get("/", function(req, res) {
     res.redirect("/tasks")
 })
---
+++ app/models/task.js
@@ -0,0 +1,24 @@
+// tasks objects
+
+var tasks = [{
+    id: 1,
+    body: "todo1",
+    completed: true
+}, {
+    id: 2,
+    body: "todo2",
+    completed: true
+}, {
+    id: 3,
+    body: "todo3",
+    completed: false
+}, {
+    id: 4,
+    body: "todo4",
+    completed: false
+}, {
+    id: 5,
+    body: "todo5",
+    completed: true
+}]
+
