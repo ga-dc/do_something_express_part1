bbf16f0f576fce84a1e62449966776fce412bd60
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-08-18 20:29:40 -0400

    created the files needed for homework part2

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var express = require("express")
 var app = express()
+app.set("view engine", "hbs")
+var tasks = [
+  {id: 1, body:"To-do 1", completed: "true" },
+  {id: 2, body:"To-do 2", completed: "true" },
+  {id: 3, body:"To-do 3", completed: "true" },
+  {id: 4, body:"To-do 4", completed: "true" },
+  {id: 5, body:"To-do 5", completed: "true" }
+]
 
-app.get('/', function(req, res){
-  res.send('Hello Cheikh');
+app.get('/tasks', function(req, res){
+  res.json('Lots of Tasks to do');
 });
+
+app.get('/tasks/:id', function(req, res){
+
+});
+
+app.post("/tasks/body", function(req, res){
+  res.json(req.tasks.body)
+})
 
 app.listen(4000, function(){
   console.log("app listening on port 4000")
 })
 
-module.exports = "express"
+module.exports = "Tasks"
