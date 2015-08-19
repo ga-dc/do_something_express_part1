979bcdac2fe1a6b03658bf7fb3056feda243f0e8
MikeDulik <mikedulik@gmail.com>
2015-08-19 08:28:36 -0400

    not complete

---
+++ app/models/task.js
@@ -0,0 +1,5 @@
+var tasks = [{id: 0, body: "a", completed: false},
+            {id: 1, body: "b", completed: false},
+            {id: 2, body: "c", completed: false},
+            {id: 3, body: "d", completed: false},
+            {id: 4, body: "e", completed: false}];
