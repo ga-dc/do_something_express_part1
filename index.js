be9ef8d2490dc40f9eec48f166cd29424b19fb27
ktw1222 <ktw1222@gmail.com>
2015-08-18 21:39:54 -0400

    added task variable

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.listen(4000, function(){
   console.log("app listening on port 4000")
 })
 
+var tasks = [
+  {id: 1, body:"buy a milk",completed: true},
+  {id: 2, body:"wash the dishes",completed: true},
+  {id: 3, body:"clean my room",completed: true},
+  {id: 4, body:"do laundry",completed: false},
+  {id: 5, body:"renew the license",completed: false}
+]
+
 app.get("/", function(req, res){
   res.send("hello world")
 })
