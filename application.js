3973ac22c8f2cfc9e12226995a04c0f5fde08656
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-08-18 22:45:03 -0400

    adding folders

--- application.js
+++ application.js
@@ -2,6 +2,8 @@ var express = require("express")
 var app = express()
 
 
+app.get()
+
 app.listen(4000, function(){
   console.log("app listening on port 4000")
 })
---
+++ controllers/tasksController.js
@@ -0,0 +1,8 @@
+var tasks = []
+for(var i=0; i<6; i++){
+  tasks[i] = {
+    id: i,
+    body: "",
+    completed: false
+  }
+}
