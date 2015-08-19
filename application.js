ed7f9943e225dfd22a8a1c90c309dd4bd8c6905b
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-08-18 23:24:19 -0400

    getting it to render a view

--- application.js
+++ application.js
@@ -2,7 +2,10 @@ var express = require("express")
 var app = express()
 
 
-app.get()
+app.get("/", function(req, res){
+  console.log("we're at root");
+  res.sendFile(__dirname + "/app/views/index.html");
+})
 
 app.listen(4000, function(){
   console.log("app listening on port 4000")
